8317680130e6cc482f1fb2687f4f5d2e16c87d36
arjun3806beai25-web <arjun3806.beai25@chitkara.edu.in>
2026-07-20 12:30:00 +0530

    Time: 1 ms (97.52%), Space: 42.4 MB (90.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0||x==1)
-        return x;
-        int start =1;
-        int end =x;
-        int ans =0;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-           long square = (long)mid*mid;
-            if(square==x)
-           return mid;
-            else if(square<x){
-            ans = mid;
-            start = mid+1;}
-            else
-            end = mid-1;
-          
-        }
-            return (int) ans;
+       if(x==0 || x==1)
+       return x;
+       int s = 1;
+       int e = x/2;
 
-        
+       while(s<=e){
+        int mid = s + (e-s)/2;
+       
+        if(mid==x/mid){
+            return mid;
+
+        }
+        else if(mid<x/mid){
+           s = mid+1; }
+            else 
+           e = mid-1;
+       }
+
+        return e;
     }
 }
