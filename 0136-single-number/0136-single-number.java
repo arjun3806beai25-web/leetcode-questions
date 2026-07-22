ab009a928e814849ba4a5f10399906aa56976001
arjun3806beai25-web <arjun3806.beai25@chitkara.edu.in>
2026-07-22 20:50:55 +0530

    Time: 1 ms (99.96%), Space: 46.8 MB (65.19%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
       
+        int sol =0;
         for(int i =0;i<nums.length;i++){
-            int count =0;
-            for(int j =0;j<nums.length;j++){
-                if(nums[i]==nums[j])
-                count++;
-
-            } 
-             if(count==1)
-      return nums[i];
-        
+            sol = sol^nums[i];
         }
-        return -1;
+        return sol;
     
     }
 }
