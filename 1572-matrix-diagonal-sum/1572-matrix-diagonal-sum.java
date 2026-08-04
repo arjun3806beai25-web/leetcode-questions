dec3be6a4b6ce1fbc51ccbfd90638c6040c7fcf7
arjun3806beai25-web <arjun3806.beai25@chitkara.edu.in>
2026-08-04 12:50:05 +0530

    Time: 1 ms (35.68%), Space: 46.5 MB (32.1%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
         int m = mat.length;
-        int n = mat[0].length;
+        int n = mat.length;
         int sum =0;
         for(int i =0;i<m;i++){
             for(int j = 0;j<n;j++){
